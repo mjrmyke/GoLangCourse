af683d21d835a2e64db3352e11f0329a6b643612
Myke Walker <mjrmyke@gmail.com>
2016-01-30 17:08:05 -0800

    Fixed equation
    Fixed equation for the project euler problem.

--- 3 - Challenges Fizzbuzz/13 Special Pythagorean triplet/main.go
+++ 3 - Challenges Fizzbuzz/13 Special Pythagorean triplet/main.go
@@ -3,7 +3,7 @@
 //Problem Description: Pythagorean triplet is a set of numbers where a^2 + b^2 = c^2
 //										 There is only one pythagorean triplet in which a+b+c = 1000
 //										 find the variables in which a+b+c = 1000, then multiple them together.
-//Output: 250800
+//Output: 31875000
 
 package main
 
@@ -16,7 +16,7 @@ func foo() int {
 	for a := 1; a < 1001; a++ {
 		for b := a + 1; b < 1001; b++ {
 			c := max - a - b
-			if a*a+b+b == c*c {
+			if a*a+b*b == c*c {
 				return (a * b * c)
 			}
 

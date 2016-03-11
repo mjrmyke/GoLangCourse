8fdea3c0d944bcf84373d2a840abe081bcdcc14a
Myke Walker <mjrmyke@gmail.com>
2016-03-10 22:45:08 -0800

    Form Data to Cookie implementation

---
+++ 19 - Form Data saved in Cookie/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nu7hatch/gouuid"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+type User struct {
+	FName string
+	LName string
+}
+
+func main() {
+
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+
+	useroni := User{}
+
+	templ, error := template.ParseFiles("tpl.gohtml") // Parse template file
+	if error != nil {
+		log.Fatalln(error)
+	}
+
+	error = templ.Execute(os.Stdout, nil)
+	if error != nil {
+		log.Fatalln(error)
+	}
+
+	cookie, err := req.Cookie("session-fino")
+
+	useroni.FName = req.FormValue("FName")
+	useroni.LName = req.FormValue("LName")
+
+	if err != nil {
+		id, _ := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:  "session-fino",
+			Value: id.String() + "_" + useroni.FName + "_" + useroni.LName,
+			// Secure: true,
+			HttpOnly: true,
+		}
+		http.SetCookie(res, cookie)
+	}
+	fmt.Println(cookie)
+
+	templ.ExecuteTemplate(res, "tpl.gohtml", useroni)
+
+}

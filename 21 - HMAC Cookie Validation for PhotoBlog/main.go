37bda33d01cf2ae2586641ab5cdf89f478daf24d
Myke <mjrmyke@gmail.com>
2016-03-14 10:42:21 -0700

    actually fixed hmac validation

--- 21 - HMAC Cookie Validation for PhotoBlog/main.go
+++ 21 - HMAC Cookie Validation for PhotoBlog/main.go
@@ -31,7 +31,7 @@ func main() {
 }
 
 func makehmac(data string) string {
-	x := hmac.New(sha256.New, []byte("key"))
+	x := hmac.New(sha256.New, []byte("salt"))
 	io.WriteString(x, data)
 	return fmt.Sprintf("%x", x.Sum(nil))
 }
@@ -47,9 +47,6 @@ func foo(res http.ResponseWriter, req *http.Request) {
 		log.Fatalln(error)
 	}
 
-	//make uuid
-	id, _ := uuid.NewV4()
-
 	cookie, err := req.Cookie("session-fino")
 
 	useroni := User{}
@@ -57,15 +54,17 @@ func foo(res http.ResponseWriter, req *http.Request) {
 	//if no cookie
 	if err != nil {
 
+		//make uuid
+		id, _ := uuid.NewV4()
 		//get data from form
 		useroni.FName = req.FormValue("FName")
 		useroni.LName = req.FormValue("LName")
 		useroni.Age = req.FormValue("Age")
-		useroni.HMAC = makehmac(id.String())
+		useroni.HMAC = makehmac(useroni.FName)
 		useroni.uuid = id
 
 		//encode data to json
-		stringdata := useroni.FName + "|" + useroni.LName + "|" + useroni.Age + "|" + useroni.uuid.String()
+		stringdata := useroni.FName + "|" + useroni.LName + "|" + useroni.Age + "|" + useroni.uuid.String() + "|" + useroni.HMAC
 		data, error := json.Marshal(stringdata)
 		if error != nil {
 			log.Fatalln(error)
@@ -100,11 +99,15 @@ func foo(res http.ResponseWriter, req *http.Request) {
 		rcvdfname := splitstrings[0]
 		rcvdlname := splitstrings[1]
 		rcvdage := splitstrings[2]
-		rcvdid := splitstrings[3]
-		codeCheck := makehmac(id.String())
+		// rcvdid := splitstrings[3]
+		rcvdhmac := splitstrings[4]
+		codeCheck := makehmac(rcvdfname)
 
-		if codeCheck != rcvdid {
+		if codeCheck != rcvdhmac {
 			log.Println("MISMATCHED UUID HMAC")
+			log.Println(rcvdfname)
+			log.Println(rcvdhmac)
+			log.Println(codeCheck)
 		}
 		log.Println(rcvdfname + rcvdlname + rcvdage)
 
